ca405b14d6b21b13842a0aa7cfe0b4b3041fa621
Austin Haas <austin@pettomato.com>
2016-12-11 12:42:37 -0800

    Add support for buttons under videos.

--- js/carousel.js
+++ js/carousel.js
@@ -9,7 +9,7 @@ function maybeOpenDrawer (carousel, drawer){
 
 function closeDrawer (target){
   $(target).hide();
-  $(target).find('.drawer-content').html("");
+  $(target).find('.content').html("");
 }
 
 function loadVideoContent (target, title, url, width, height){
@@ -27,6 +27,16 @@ function loadImageContent (target, title, url, width, height){
 
 function loadHTMLContent (target, title, url, width, height){
   target.load(url.concat(" .container-fluid"));
+}
+
+function loadButtons (target, content){
+  target.empty();
+  for (var i=0; i<content.length; i++){
+    var button = content[i];
+    target.append($('<a></a>')
+                  .html(button.title)
+                  .attr("href", button.url));
+  }
 }
 
 $(document).ready(function(){
@@ -76,7 +86,8 @@ $(document).ready(function(){
       var buttons  = carousel.find(".slick-carousel .thumb a");
       var drawer   = carousel.find(".drawer");
       var close    = drawer.find(".close-button");
-      var target   = drawer.find('.drawer-content');
+      var target   = drawer.find('.content');
+      var button_target = drawer.find('.buttons');
 
       buttons.each(function (){
 
@@ -86,10 +97,22 @@ $(document).ready(function(){
         var width  = button.data('width');
         var height = button.data('height');
 
+        // This is a hack, because I couldn't figure out how to pass
+        // an array via data attributes.
+        var button_data = [];
+        for(var i=1; i<20; i++){
+          var button_title = button.data('button-' + i + '-title');
+          var button_url = button.data('button-' + i + '-url');
+          if( button_title && button_url ){
+            button_data.push({title: button_title, url: button_url});
+          }
+        }
+
         switch (button.data('type')) {
         case "video":
           button.click(function(){
             loadVideoContent(target, title, url, width, height);
+            loadButtons(button_target, button_data);
             $(drawer).css('max-width', width);
             maybeOpenDrawer(carousel, drawer);
             return false;
@@ -98,6 +121,7 @@ $(document).ready(function(){
         case "image":
           button.click(function(){
             loadImageContent(target, title, url, width, height);
+            loadButtons(button_target, button_data);
             $(drawer).css('max-width', '50%');
             maybeOpenDrawer(carousel, drawer);
             return false;
@@ -106,6 +130,7 @@ $(document).ready(function(){
         case "html":
           button.click(function(){
             loadHTMLContent(target, title, url, width, height);
+            loadButtons(button_target, button_data);
             $(drawer).css('max-width', '100%');
             maybeOpenDrawer(carousel, drawer);
             return false;
